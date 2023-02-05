c0d3e1a1233e5f2a78236f971cd56c3430063d9b
Anderson de Souza <anderson17ads@hotmail.com>
2023-02-05 00:43:28 -0300

    Teste de deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.send(200);
+  return response.send('Test Deploy!');
 });
 
 app.listen(process.env.PORT || 3000);
