495649ede9cfe8d61fc1f8bee33c110d58af7b12
Anderson de Souza <anderson17ads@hotmail.com>
2023-02-06 16:41:18 -0300

    Adicionando o bodyParser; Ajustando rota inicial para receber post

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 require('dotenv/config');
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import conversations from './conversations';
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+app.post('/', (request, response) => {
   conversations();
 
   return response.send('Test!');
